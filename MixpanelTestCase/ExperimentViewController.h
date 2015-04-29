efcad841f18d82dbfd8d11f6bef54ba761b64037
Brad Fol <bradfol@gmail.com>
2015-04-29 13:49:26 -0700

    Create ExperimentViewController class files

---
+++ MixpanelTestCase/ExperimentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExperimentViewController.h
+//  MixpanelTestCase
+//
+//  Created by Brad Fol on 4/29/15.
+//  Copyright (c) 2015 Brad Fol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExperimentViewController : UIViewController
+
+@end
---
+++ MixpanelTestCase/ExperimentViewController.m
@@ -0,0 +1,22 @@
+//
+//  ExperimentViewController.m
+//  MixpanelTestCase
+//
+//  Created by Brad Fol on 4/29/15.
+//  Copyright (c) 2015 Brad Fol. All rights reserved.
+//
+
+#import "ExperimentViewController.h"
+
+@interface ExperimentViewController ()
+
+@end
+
+@implementation ExperimentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
